2634507fe20bd81b72fe942b26c13d25f0143065
Mohammad Keshavarz <mohammadkeshavarz.a.13@gmail.com>
2024-12-05 21:07:36 +0330

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import About from "@/components/general/about";
 import Hero from "@/components/general/hero";
+import NavBar from "@/components/general/navbar";
 import Slack from "@/components/layout/slack";
 import Title from "@/components/typo/title";
 
 const Home = () => {
     return (
         <section className="w-full flex flex-col overflow-y-auto bg-white">
+            <NavBar />
             <Hero />
             <About />
         </section>
