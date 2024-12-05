5f2966176ed5b26aa547e0310d3675c3da8d4d76
Mohammad Keshavarz <mohammadkeshavarz.a.13@gmail.com>
2024-12-05 21:04:00 +0330

    add about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/general/about";
 import Hero from "@/components/general/hero";
 import Slack from "@/components/layout/slack";
 import Title from "@/components/typo/title";
@@ -6,7 +7,7 @@ const Home = () => {
     return (
         <section className="w-full flex flex-col overflow-y-auto bg-white">
             <Hero />
-            <section className="w-full h-dvh bg-red-500"></section>
+            <About />
         </section>
     );
 };
